52390623470dea3050319d36a82297b2aaf156e0
Luca Beetz <luca-beetz@tutanota.com>
2023-12-28 03:14:51 +0100

    lots of changes

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,16 +1,32 @@
 use std::io;
+use std::io::Write;
 
 pub enum State {
-    Closed,
-    Listen,
     SynRcvd,
     Estab,
+    FinWait1,
+    FinWait2,
+    Closing,
+    TimeWait,
+}
+
+impl State {
+    fn is_synchronized(&self) -> bool {
+        match self {
+            State::SynRcvd => false,
+            State::Estab | State::FinWait1 | State::Closing | State::FinWait2 | State::TimeWait => {
+                true
+            }
+        }
+    }
 }
 
 pub struct Connection {
     state: State,
     send: SendSequenceSpace,
     recv: ReceiveSequenceSpace,
+    ip: etherparse::Ipv4Header,
+    tcp: etherparse::TcpHeader,
 }
 
 /// Send Sequence Space (RFC 793 S3.2 F4)
@@ -97,60 +113,219 @@ impl Connection {
                 wnd: tcp_header.window_size(),
                 up: false,
             },
+            ip: etherparse::Ipv4Header::new(
+                0,
+                64,
+                etherparse::IpNumber::Tcp as u8,
+                [
+                    ip_header.destination()[0],
+                    ip_header.destination()[1],
+                    ip_header.destination()[2],
+                    ip_header.destination()[3],
+                ],
+                [
+                    ip_header.source()[0],
+                    ip_header.source()[1],
+                    ip_header.source()[2],
+                    ip_header.source()[3],
+                ],
+            ),
+            tcp: etherparse::TcpHeader::new(
+                tcp_header.destination_port(),
+                tcp_header.source_port(),
+                iss,
+                10,
+            ),
         };
 
-        // send SYN-ACK
-        let mut syn_ack = etherparse::TcpHeader::new(
-            tcp_header.destination_port(),
-            tcp_header.source_port(),
-            c.send.iss,
-            c.send.wnd,
-        );
-        syn_ack.acknowledgment_number = c.recv.nxt;
-        syn_ack.syn = true;
-        syn_ack.ack = true;
-
-        let ip = etherparse::Ipv4Header::new(
-            syn_ack.header_len(),
-            64,
-            etherparse::IpNumber::Tcp as u8,
-            [
-                ip_header.destination()[0],
-                ip_header.destination()[1],
-                ip_header.destination()[2],
-                ip_header.destination()[3],
-            ],
-            [
-                ip_header.source()[0],
-                ip_header.source()[1],
-                ip_header.source()[2],
-                ip_header.source()[3],
-            ],
-        );
-
-        // write headers
-        let unwritten = {
-            let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten);
-            syn_ack.write(&mut unwritten);
-            unwritten.len()
-        };
-        // send flags and proto first (again 0x2 because of macos)
-        let mut new_buf = vec![0, 0, 0, 2];
-        new_buf.extend_from_slice(&buf[..buf.len() - unwritten]);
-        nic.write(&new_buf).unwrap();
-
-        eprintln!("responding with {:02x?}", &buf[..buf.len() - unwritten]);
-
+        c.tcp.syn = true;
+        c.tcp.ack = true;
+        c.write(nic, &[])?;
         Ok(Some(c))
     }
+
+    fn write(
+        &mut self,
+        nic: &mut dyn tun::Device<Queue = tun::platform::Queue>,
+        payload: &[u8],
+    ) -> io::Result<usize> {
+        let mut buf = [0u8; 1500];
+        self.tcp.sequence_number = self.send.nxt;
+        self.tcp.acknowledgment_number = self.recv.nxt;
+
+        let size = std::cmp::min(
+            buf.len(),
+            self.tcp.header_len() as usize + self.ip.header_len() as usize + payload.len(),
+        );
+        self.ip
+            .set_payload_len(self.tcp.header_len() as usize + payload.len());
+
+        let mut unwritten = &mut buf[..];
+        self.ip.write(&mut unwritten);
+        self.tcp.write(&mut unwritten)?;
+        let payload_bytes = unwritten.write(payload)?;
+        let unwritten = unwritten.len();
+
+        // write packet flags and protocol first
+        let mut new_buf = vec![0, 0, 0, 2];
+        new_buf.extend_from_slice(&buf[..buf.len() - unwritten]);
+
+        self.send.nxt = self.send.nxt.wrapping_add(payload_bytes as u32);
+        if self.tcp.syn {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.tcp.syn = false;
+        }
+        if self.tcp.fin {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.tcp.fin = false;
+        }
+
+        nic.write(&new_buf).unwrap();
+        Ok(payload.len())
+    }
+
+    fn send_rst(
+        &mut self,
+        nic: &mut dyn tun::Device<Queue = tun::platform::Queue>,
+    ) -> io::Result<()> {
+        self.tcp.rst = true;
+        self.tcp.sequence_number = 0;
+        self.tcp.acknowledgment_number = 0;
+        self.ip.set_payload_len(self.tcp.header_len() as usize);
+        self.write(nic, &[])?;
+        Ok(())
+    }
+
     pub fn on_packet(
         &mut self,
         nic: &mut dyn tun::Device<Queue = tun::platform::Queue>,
         ip_header: etherparse::Ipv4HeaderSlice,
         tcp_header: etherparse::TcpHeaderSlice,
         data: &[u8],
-    ) -> io::Result<usize> {
-        Ok(0)
+    ) -> io::Result<()> {
+        // valid segment check
+        // RCV.NXT <= SEG.SEQ < RCV.NXT + RCV.WND
+        let seqn = tcp_header.sequence_number();
+        let mut slen = data.len() as u32;
+        if tcp_header.fin() {
+            slen += 1;
+        }
+        if tcp_header.syn() {
+            slen += 1;
+        }
+        let wend = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
+
+        if slen == 0 {
+            // zero length segment
+            if self.recv.wnd == 0 {
+                if seqn != self.recv.nxt {
+                    return Ok(());
+                }
+            } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend) {
+                return Ok(());
+            }
+        } else {
+            if self.recv.wnd == 0 {
+                return Ok(());
+            } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend)
+                && !is_between_wrapped(
+                    self.recv.nxt.wrapping_sub(1),
+                    seqn.wrapping_add(slen - 1),
+                    wend,
+                )
+            {
+                return Ok(());
+            }
+        }
+
+        if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend)
+            && !is_between_wrapped(
+                self.recv.nxt.wrapping_sub(1),
+                seqn + data.len() as u32 - 1,
+                wend,
+            )
+        {
+            return Ok(());
+        }
+        self.recv.nxt = seqn.wrapping_add(slen);
+        // TODO: if not acceptable, send ACK
+
+        if !tcp_header.ack() {
+            return Ok(());
+        }
+
+        let ackn = tcp_header.acknowledgment_number();
+        if let State::SynRcvd = self.state {
+            if !is_between_wrapped(
+                self.send.una.wrapping_sub(1),
+                ackn,
+                self.send.nxt.wrapping_add(1),
+            ) {
+                self.state = State::Estab;
+            } else {
+                // TODO: reset
+            }
+        }
+        // terminate the connection
+        // TODO: don't save it on the header, should only be set for last packet
+        // self.tcp.fin = true;
+        // self.write(nic, &[])?;
+        // self.state = State::FinWait1;
+
+        if let State::Estab = self.state {
+            if !is_between_wrapped(self.send.una, ackn, self.send.nxt.wrapping_add(1)) {
+                return Ok(());
+            }
+            self.send.una = ackn;
+            // TODO
+            assert!(data.is_empty());
+
+            // terminate the connection
+            // TODO: don't save it on the header, should only be set for last packet
+            self.tcp.fin = true;
+            self.write(nic, &[])?;
+            self.state = State::FinWait1;
+        }
+
+        if let State::FinWait1 = self.state {
+            if self.send.una == self.send.iss + 2 {
+                self.state = State::FinWait2;
+            }
+        }
+
+        if tcp_header.fin() {
+            match self.state {
+                State::FinWait2 => {
+                    // done with connection
+                    self.write(nic, &[])?;
+                    self.state = State::TimeWait;
+                }
+                _ => unimplemented!(),
+            }
+        }
+
+        Ok(())
     }
 }
+
+fn is_between_wrapped(start: u32, x: u32, end: u32) -> bool {
+    use std::cmp::Ordering;
+    match start.cmp(&x) {
+        Ordering::Equal => (),
+        Ordering::Less => {
+            if end >= start && end <= x {
+                return false;
+            }
+        }
+        Ordering::Greater => {
+            if end < start && end > x {
+            } else {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+// continue 4:35
